bf2a22d8ed8158e2409b92d0bdebf6622faf1f01
navdeepsingh160 <118738353+navdeepsingh160@users.noreply.github.com>
2024-02-06 16:52:12 +0530

    Update project160.py

--- project160.py
+++ project160.py
@@ -2,6 +2,7 @@ from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
 import os
+from tkinter import messagebox
 
 root = Tk()
 root.minsize(650,650)
@@ -27,20 +28,37 @@ def openfile():
     global name
     my_text.delete(1.0,END)
     input_file_name.delete(0,END)
-    textfile = filedialog.askopenfilename(title = " Open Text File",
+    text_file = filedialog.askopenfilename(title = " Open Text File",
                                           filetypes = (("text files", "*.txt"),))
     print (text_file)
     name = os.path.basename(text_file)
     formated_name = name.split(".")[0]
-    inputfilename.insert(END, formated_name)
+    input_file_name.insert(END, formated_name)
     root.title(formated_name)
     text_file = open(name,'r')
     paragarph = text_file.read()
     my_text.insert(END,paragarph)
     textfile.close()
+def  save():
+    input_name = input_file_name.get()
+    file = open(input_name+".txt","w")
+    data = my_text.get("1.0",END)
+    print(data)
+    file.write(data)
+    input_file_name.delete(0, END)
+    my_text.delete(1.0, END)
+    messagebox.showinfo("update", "success")
+    
+def closewindow():
+    root.destroy() 
 
 
 open_button=Button(root,image = open_img, text= "openfile", command=openfile)
 open_button.place(relx=0.05,rely=0.03,anchor=CENTER)
+save_button=Button(root,image = save_img, text= "savefile", command=save)
+save_button.place(relx=0.11,rely=0.03,anchor=CENTER)
+exit_button=Button(root,image = run_img, text= "runfile", command=closewindow)
+exit_button.place(relx=0.17,rely=0.03,anchor=CENTER)
+
 
 root.mainloop()
